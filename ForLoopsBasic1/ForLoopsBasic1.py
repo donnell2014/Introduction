4e87510d7b00c9e9fda0805e02b688915c396fb7
Donnell Albertie <dalbertie5@gmail.com>
2021-05-04 11:27:06 -0500

    commit

---
+++ ForLoopsBasic1/ForLoopsBasic1.py
@@ -0,0 +1,29 @@
+# 1.
+for x in range(151):
+    print(x)
+#2.
+for x in range(0,1001,5):
+    print(x)
+# 3.
+for x in range(0,101,5):
+    if x % 10==0:
+        print("Coding Dojo")
+    elif x %5==0:
+        print("Coding")
+# 4.
+total=0
+for x in range(0,500001,1):
+    if x%2==1:
+        print(x)
+        total= x + total
+
+print (total)
+#5.
+for x in range(2018,0,-4):
+    print(x)
+#6.
+lownum=3
+highnum=66
+mult=3
+for x in range(3,67,3):
+    print(x)
